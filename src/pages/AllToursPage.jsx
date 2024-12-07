c1a7aa8c3811e62b71f8ec08908ee2504258fda8
cathodert <catherineblentweyne@gmail.com>
2024-12-07 09:37:21 +0800

    updated AllPages css

--- src/pages/AllToursPage.jsx
+++ src/pages/AllToursPage.jsx
@@ -2,27 +2,27 @@ import useTours from "../hooks/use-tours";
 import TourCard from "../components/TourCard";
 import CreateTour from "../components/TourCreate";
 import { useState } from "react";
-import "./pages.css";
+import "./Pages.css";
 import "../components/Form.css"
 
 function AllToursPage() {
   const { tours } = useTours();
-  const[displayForm, setDisplayForm] = useState(false)  
-  const showForm = () => {setDisplayForm(true)}
+  // const[displayForm, setDisplayForm] = useState(false)  
+  // const showForm = () => {setDisplayForm(true)}
 
   return (
     <div>
       <div className="hero">
         <h1 >See all tours</h1>
       </div>
-      <div className="display-form"> 
+      {/* <div className="display-form"> 
        { !displayForm ? <button type="Display-form" onClick={showForm}>
                 Add a new tour
             </button> : null}
             </div>
         <div className="contact-form">
           {displayForm ? <CreateTour/> : null}
-        </div>
+        </div> */}
       <div id="tour-list">
           {tours.map((tourData, key) => {
             return <TourCard key={key} tourData={tourData} />;
